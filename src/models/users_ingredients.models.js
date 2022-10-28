6ed3ef51b8eda1c87a3ac02691ba29020a9d5cb9
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-10-28 12:10:55 -0500

    models recipes and ingredients

---
+++ src/models/users_ingredients.models.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+
+const db = require("../utils/database");
+const Users = require("./users.models");
+const Ingredients = require("./ingredients.models");
+
+const UsersIngredients = db.define("recipes", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    field: "user_id",
+    references: {
+      key: "id",
+      model: Users,
+    },
+  },
+  ingredientId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    field: "ingredient_id",
+    references: {
+      key: "id",
+      model: Ingredients,
+    },
+  },
+});
+
+module.exports = UsersIngredients;
